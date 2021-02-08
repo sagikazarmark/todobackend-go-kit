5b05f6c896484336e8de583495ca06d68741b2e4
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-02-08 11:46:54 +0100

    chore: regenerate proto
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- api/todo/v1/todo_list_grpc.pb.go
+++ api/todo/v1/todo_list_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
+// Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
 // TodoListServiceClient is the client API for TodoListService service.
@@ -144,7 +145,7 @@ type UnsafeTodoListServiceServer interface {
 }
 
 func RegisterTodoListServiceServer(s grpc.ServiceRegistrar, srv TodoListServiceServer) {
-	s.RegisterService(&_TodoListService_serviceDesc, srv)
+	s.RegisterService(&TodoListService_ServiceDesc, srv)
 }
 
 func _TodoListService_AddItem_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -255,7 +256,10 @@ func _TodoListService_DeleteItem_Handler(srv interface{}, ctx context.Context, d
 	return interceptor(ctx, in, info, handler)
 }
 
-var _TodoListService_serviceDesc = grpc.ServiceDesc{
+// TodoListService_ServiceDesc is the grpc.ServiceDesc for TodoListService service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var TodoListService_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "todo.v1.TodoListService",
 	HandlerType: (*TodoListServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
