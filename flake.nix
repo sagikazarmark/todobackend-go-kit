5465b27f66b133767fcd323be0c5fa663734d1b9
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2022-05-13 01:05:41 +0200

    chore: update flake
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -6,28 +6,41 @@
     flake-utils.url = "github:numtide/flake-utils";
     goflake.url = "github:sagikazarmark/go-flake";
     goflake.inputs.nixpkgs.follows = "nixpkgs";
+    gobin.url = "github:sagikazarmark/go-bin-flake";
+    gobin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, goflake, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs = { self, nixpkgs, flake-utils, goflake, gobin, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
 
-          overlays = [ goflake.overlay ];
-        };
-        buildDeps = with pkgs; [ git go_1_17 gnumake ];
-        devDeps = with pkgs; buildDeps ++ [
-          golangci-lint
-          gotestsum
-          goreleaser
-          protobuf
-          protoc-gen-go
-          protoc-gen-go-grpc
-          protoc-gen-kit
-          # gqlgen
-          openapi-generator-cli
-        ];
-      in
-      { devShell = pkgs.mkShell { buildInputs = devDeps; }; });
+            overlays = [
+              goflake.overlay
+
+              (
+                final: prev: {
+                  golangci-lint = gobin.packages.${system}.golangci-lint-bin;
+                }
+              )
+            ];
+          };
+
+          buildDeps = with pkgs; [ git go_1_17 gnumake ];
+          devDeps = with pkgs; buildDeps ++ [
+            golangci-lint
+            gotestsum
+            goreleaser
+            protobuf
+            protoc-gen-go
+            protoc-gen-go-grpc
+            protoc-gen-kit
+            # gqlgen
+            openapi-generator-cli
+          ];
+        in
+          { devShell = pkgs.mkShell { buildInputs = devDeps; }; }
+    );
 }
